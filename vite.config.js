728c3f2ac88ad1745b89a75c256594203d760ea9
olehpf <olehpf@gmail.com>
2025-05-27 15:38:48 +0300

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-// import tailwindcss from "@tailwindcss/vite"; // (див. нижче)
+
 export default defineConfig({
-  // plugins: [ tailwindcss() ], // (див. нижче)
   server: {
     proxy: {
       '/api': 'http://localhost:3010'
